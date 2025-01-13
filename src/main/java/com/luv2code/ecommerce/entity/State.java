2f87fe8d7eaa099d683e1d35ce9c1f442864f3da
Alison Wu <alison.wu23@gmail.com>
2025-01-13 14:27:12 +1000

    feat: ditto

---
+++ src/main/java/com/luv2code/ecommerce/entity/State.java
@@ -0,0 +1,24 @@
+package com.luv2code.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "state")
+@Data
+public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+}
